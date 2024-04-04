25f23c62f45f7aa015b6cb422a4512288352918d
omasanetsano <etsanoomasanjuwa@gmail.com>
2024-04-04 12:55:12 +0100

    change of color

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ else {
     let li=document.createElement("li");
     li.innerHTML=inputBox.value;
     listContainer.appendChild(li);
-    let span = document.createElement("span");
+    let span = document.createElement("span"); 
     span.innerHTML="\u00d7";
     li.appendChild(span);
 }
